1446e744d8b45e7c866d224e6f7d821a6bb8c1bb
Tim Reilly <timreilly@live.com>
2017-06-21 16:41:53 -0700

    working from service bus to function

--- NotificationConsole/Program.cs
+++ NotificationConsole/Program.cs
@@ -4,18 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.NotificationHubs;
+using Microsoft.ServiceBus.Messaging;
 
 
 namespace NotificationConsole
 {
     class Program
     {
+
+        private const string ServiceBusConnectionString = "Endpoint=sb://trackbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=IPVbf690OcTx+RLgqA+N8fPQofb9i9SraxpQ7qB9R6s=";
+        private const string QueueName = "";
+        public static TopicClient Client = TopicClient.CreateFromConnectionString(ServiceBusConnectionString, "warnings");
+
         static void Main(string[] args)
         {
-            var words = Console.ReadLine(); 
+            var words = Console.ReadLine();
+            if (words == "s")
+            {
+                Console.WriteLine("what do you want to put in Service Bus? :");
+                var w = Console.ReadLine();
+
+                SendToSB(w);
+            }
             SendNotificationAsync(words);
             Console.ReadLine();
 
+
+        }
+
+        private static async void SendToSB(string words)
+        {
+            BrokeredMessage message = new BrokeredMessage("Test: " + words);
+            await Client.SendAsync(message); 
 
         }
 
@@ -34,6 +54,9 @@ namespace NotificationConsole
             var toast = @"<toast><visual><binding template=""ToastText01""><text id=""1"">" + words + @"</text></binding></visual></toast>";
             await hub.SendWindowsNativeNotificationAsync(toast);
         }
+
+
+
 
     }
 }
